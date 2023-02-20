7e2570f975a954b66f4fb36405ec56deb2cac949
Tim <github@timgreen.ws>
2023-02-20 14:10:53 +1100

    config tailwind to work with project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,12 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: require("fast-glob").sync(["./**/*.php", "*.php"]),
-  safelist: [
-    {
-      pattern: /text-(lg|sm|xs|[1-9]xl)/,
-    },
-    {
-      pattern: /(.*)primary(.*)/,
-    },
-    {
-      pattern: /(.*)secondary(.*)/,
-    }
-  ],
+  content: ["./**/*.php"],
   theme: {
     extend: {
       colors: {
-        primary: "#9EFF00",
-        secondary: "#3B009C"
+        primary: "#aaff1f",
+        secondary: "#6c26de"
       },
     },
   },
   plugins: [],
-};
+}
