b123f539bd31bba6732c3a628f7b129e7a4faafe
Tim <github@timgreen.ws>
2023-02-27 11:57:36 +1100

    created navbar and navbar general markcup

---
+++ template-parts/elements/navbar.php
@@ -0,0 +1,16 @@
+<div class="shock-navbar-wrapper fixed z-50 top-0 left-0 flex w-full<?= ($args["isFrontpage"]) ? "" : " bg-ocean" ?>">
+    <div class="md-container shock-navbar-inner flex justify-between w-full py-2 items-center">
+        <div class="shock-navbar-logo flex justify-center">
+            <a class="shock-logo shock-noline shock-bigshoulders py-3 px-4 bg-primary text-secondary leading-none text-4xl text-center border-2 border-secondary" href="/">Shock</a>
+        </div>
+        <div class="shock-navbar-menu flex justify-end">
+            <div class="shock-navbar-menubutton border-2 border-secondary bg-secondary">
+                <button class="shock-navbar-tofuburger">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
