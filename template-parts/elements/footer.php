5fa885de23698e776c4f87f8e7a01944b934cfb6
Tim <github@timgreen.ws>
2023-02-27 11:58:45 +1100

    footer created and markup added

---
+++ template-parts/elements/footer.php
@@ -0,0 +1,15 @@
+<div class="shock-footer-wrapper bg-primary mt-36">
+    <div class="shock-footer-inner flex justify-between w-full p-4 items-center">
+        <div class="shock-footer-left flex">
+            <div class="shock-footer-nav flex gap-4 text-secondary font-bold">
+                
+            </div>
+        </div>
+
+        <div class="shock-footer-right flex items-center gap-x-8">
+            <div class="shock-footer-some-icons flex gap-3">
+                
+            </div>
+        </div>
+    </div>
+</div>
