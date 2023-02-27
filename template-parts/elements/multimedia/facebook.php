355a643aed70e3a7d92ae50abbc1795fd45054db
Tim <github@timgreen.ws>
2023-02-27 13:37:54 +1100

    added facebook embed

---
+++ template-parts/elements/multimedia/facebook.php
@@ -0,0 +1,7 @@
+
+<?php
+    //for ACF
+    $id = $args["ID"]
+?>
+
+<iframe src="//www.facebook.com/plugins/post.php?=href<?php urlencode($id)?>&show_text=true" style="border:none;overflow:hidden;width:100%;" frameborder="0" scrolling="no" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
