ccafece2689d84ae66753aa354adeb4c8cace910
Tim <github@timgreen.ws>
2023-02-27 13:38:29 +1100

    added youtube embed

---
+++ template-parts/elements/multimedia/youtube.php
@@ -0,0 +1,9 @@
+
+<?php
+    //for ACF
+    $id = explode("=", $args["ID"])[1];
+?>
+
+<div class="shock-multimedia-yt">
+    <iframe src="//www.youtube.com/<?php $id ?>" title="YouTube" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+</div>
