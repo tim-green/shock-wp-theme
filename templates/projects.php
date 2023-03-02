67028dc459e40802ae5e024c38649f4c9c7d7eb5
Tim <github@timgreen.ws>
2023-03-02 17:25:42 +1100

    added wp_query

--- templates/projects.php
+++ templates/projects.php
@@ -5,4 +5,19 @@
 */
 
 get_header();
+
+$the_query = new WP_Query(array(
+    "post_type" => "projects",
+    "post_per_page" => 6,
+    "status" => "publish",
+    "order" => "DESC",
+    "paged" => get_query_var("paged") ?  get_query_var("paged") : 1
+    ));
+
+    $projects = $the_query->posts;
+    get_template_part( "template/parts/elements/page-header", "", array(
+        "titles" => get_the_title(),
+        "subtitle" => get_field("subtitle"),
+        "featured_image" => get_the_post_thumbnail_url()
+    ));
 ?>
