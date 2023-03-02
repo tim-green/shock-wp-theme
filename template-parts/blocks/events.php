d96e86e8d6af2adb1d4ab1a4155217c5433de839
Tim <github@timgreen.ws>
2023-03-02 17:08:46 +1100

    get events from tribe

--- template-parts/blocks/events.php
+++ template-parts/blocks/events.php
@@ -1,5 +1,27 @@
 <?php
 //get the current date
 $now = date('Y-m-d H:i', time());
+
+//events attr
+$events = tribe_get_events( array(
+        "start_date" => date('Y-m-d', time()),
+        'orderby'        => 'date',
+        'order'          => 'ASC',
+        'featured'       => true,
+        'posts_per_page' => 3));
+
+//get events
+if (count($events) < 3) {
+    $events_bydate = tribe_get_events(
+        array(
+        "start_date" => date('Y-m-d', time()),
+        'orderby'        => 'date',
+        'order'          => 'ASC',
+        'featured'       => false,
+        'posts_per_page' => 3 - count($events)
+    ));
+    $events = array_merge($events, $events_bydate);
+}
+?>
 <div class="shock-events-wrapper">
 </div>
