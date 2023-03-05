eac587eaccfed72c32d220d8820acfb8bb452a07
Tim <github@timgreen.ws>
2023-03-06 00:26:08 +1100

    markup for single.php

--- single.php
+++ single.php
@@ -1,40 +1,3 @@
-<?php get_header(); ?>
-
-<main id="content-wrapper">
-
-	<?php
-	while ( have_posts() ) :
-		the_post();
-		?>
-	
-	<div class="container">
-
-		<?php grnd_breadcrumbs(); ?>
-
-		<div class="row py-5">
-
-			<div id="article-wrapper" class="col">       
-
-				<?php get_template_part( 'templates/content/single', '' ); ?>
-
-				<nav class="nav">
-					<?php
-					previous_post_link( '<span class="nav-link me-auto">&laquo; %link</span>' );
-						next_post_link( '<span class="nav-link ms-auto">%link &raquo;</span>' );
-					?>
-				</nav>
-					
-				<?php
-				if ( comments_open() || get_comments_number() ) {
-					comments_template(); }
-				?>
-			
-			</div> <!-- #article-wrapper -->
-
-			<?php get_sidebar(); ?>
-
-		</div>
-
 <?php 
 get_header(); 
 get_template_part( "template/parts/elements/page-header", "", array(
@@ -43,11 +6,11 @@ get_template_part( "template/parts/elements/page-header", "", array(
 	"featured_image" => get_field("different_hero_image")["url"] ?? get_the_post_thumbnail_url()
 	));
 ?>
+
+<main class="md-container" id="shock-main-page-content">
+	<div class="shock-content-container">
+		<?php the_content(); ?>
 	</div>
-  
-	<?php endwhile ?>
+</main>
 
-</main> <!-- #content-wrapper -->
-
-<?php
-get_footer();
+<?php get_footer();?>
