861b43cc6a41dcde4ed6cf60b51846be7057466d
Tim <github@timgreen.ws>
2023-03-06 00:25:51 +1100

    get page header template part

--- single.php
+++ single.php
@@ -37,6 +37,11 @@
 
 <?php 
 get_header(); 
+get_template_part( "template/parts/elements/page-header", "", array(
+	"title" => get_the_title(),
+	"subtitle" => get_field("subtitle"),
+	"featured_image" => get_field("different_hero_image")["url"] ?? get_the_post_thumbnail_url()
+	));
 ?>
 	</div>
   
