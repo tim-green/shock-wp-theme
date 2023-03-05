b4f1ca1a1fde6ef26dc39f11315a71ab44132940
Tim <github@timgreen.ws>
2023-03-06 00:26:29 +1100

    markup for page

--- page.php
+++ page.php
@@ -1,42 +1,17 @@
 <?php get_header(); ?>
 
-<main id="content-wrapper">
+<div class="shock-hero-container pt-36 pb-12 mb-12 bgprimary">
+	<div class="hero-inner md-container">
+		<h1 class="bg-secondary">
+			<span class="text-primary"><?php the_title();?></span>
+		</h1>
+	</div>
+</div>
 
-	<?php
-	while ( have_posts() ) :
-		the_post();
+<main class="md-container" id="shock-main-page-content">
+	<div class="shock-content-container">
+		<?php the_content(); ?>
+	</div>
+</main>
 
-		if ( has_post_thumbnail() ) {
-
-			get_template_part( 'templates/sections/common', 'pageheader' );
-
-		}
-		?>
-
-		<div class="container">
-
-			<?php
-
-			grnd_breadcrumbs();
-
-			// Get this page slug
-			$slug = $post->post_name;
-
-			// Check if template file exists, set the template to be used
-			$template_name = ( is_file( get_theme_file_path( 'templates/content/page-' . $slug . '.php' ) ) ) ? $slug : '';
-
-			get_template_part( 'templates/content/page', $template_name );
-
-			if ( comments_open() || get_comments_number() ) {
-				comments_template(); }
-
-			?>
-			
-		</div>
-
-	<?php endwhile ?>
-
-</main> <!-- #content-wrapper -->
-
-<?php
-get_footer();
+<?php get_footer(); ?>
