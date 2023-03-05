beed0cd928d60ccad28457d52eba7519cfb8a033
Tim <github@timgreen.ws>
2023-03-05 11:16:36 +1100

    cleaned up front page, changed markup for theme layout

--- front-page.php
+++ front-page.php
@@ -1,68 +1,9 @@
-<?php get_header(); ?>
+<?php 
 
-<main id="content-wrapper">
+get_header(); 
 
-	<section id="section-slider">
+the_content();
 
-		<?php
-
-		$args = array(
-			'post_type' => 'post',
-			'posts_per_page'    => 3,
-		);
-
-		get_template_part( 'templates/sections/home', 'slider', $args );
-
-		?>
-
-	</section> <!-- #section-slider -->
-
-	<section id="section-services">
-
-		<?php
-
-		$args = array(
-			'post_type' => 'post',
-			'posts_per_page'    => 3,
-		);
-
-		get_template_part( 'templates/sections/home', 'services', $args );
-
-		?>
-
-	</section> <!-- #section-services -->
-
-	<section id="section-cta">
-
-		<?php
-
-		$args = array(
-			'post_type' => 'post',
-			'posts_per_page'    => 1,
-		);
-
-		get_template_part( 'templates/sections/home', 'cta', $args );
-
-		?>
-
-	</section> <!-- #section-news -->
- 
-	<section id="section-news">
-
-		<?php
-
-		$args = array(
-			'post_type' => 'post',
-			'posts_per_page'    => 3,
-		);
-
-		get_template_part( 'templates/sections/home', 'news', $args );
-
-		?>
-
-	</section> <!-- #section-news -->
- 
-</main> <!-- #content-wrapper -->
-
-<?php
 get_footer();
+
+?>
