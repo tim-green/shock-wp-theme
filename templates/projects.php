44c23df7fb9c7ac3698b74705539f43113fee80a
Tim <github@timgreen.ws>
2023-03-05 11:10:20 +1100

    added pagination

--- templates/projects.php
+++ templates/projects.php
@@ -37,6 +37,29 @@ $the_query = new WP_Query(array(
     ?>
     </div>
     <div class="shock-projects-pagination mt-8">
+        <?php 
+            echo paginate_link(
+                array(
+                    'base'  => str_replace(
+                        999999999, '%#%', esc_url(
+                            get_pagenum_link(999999999))), 'total' => $the_query->max_num_pages,
+                            'current' => max(1, get_query_var('paged')),
+                            'format' =>'?paged%#%', 
+                            'show_all' => false,
+                            'type' => 'plain',
+                            'end_size' => 2,
+                            'mid_size' => 1,
+                            'prev_next' => true,
+                            'prev_text' => sprintf('<i></i>%1$s',__('<','text-domain')),
+                            'next_text' => sprintf('%1$s<i></i>', __('>','text-domain')),
+                            'add_args' => false,
+                            'add_fragment' =>'',
+                            )
+                        );
+        ?>
     </div>
 </main>
-<?php     get_footer();?>
+
+<?php 
+    get_footer();
+?>
