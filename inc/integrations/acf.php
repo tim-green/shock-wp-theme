bffc93787cfbf34f108823534aa537d0c9ab67f4
Tim <github@timgreen.ws>
2023-03-05 11:12:06 +1100

    added function for ACF

--- inc/integrations/acf.php
+++ inc/integrations/acf.php
@@ -12,3 +12,33 @@
 // 	acf_add_options_page();
 
 // }
+
+function grnd_acf(){
+    define( 'MY_ACF_PATH', get_stylesheet_directory() . '/lib/acf/' );
+    define( 'MY_ACF_URL', get_stylesheet_directory_uri() . '/lib/acf/' );
+    include_once( MY_ACF_PATH . 'acf.php' );
+    add_filter('acf/settings/url', 'my_acf_settings_url');
+    function my_acf_settings_url( $url ) {
+        return MY_ACF_URL;
+    }
+
+    add_filter('acf/settings/save_json', 'set_acf_json_save_folder');
+    function set_acf_json_save_folder( $path ) {
+        $path = MY_ACF_PATH . '/acf-json';
+        return $path;
+    }
+    add_filter('acf/settings/load_json', 'add_acf_json_load_folder');
+    function add_acf_json_load_folder( $paths ) {
+        unset($paths[0]);
+        $paths[] = MY_ACF_PATH . '/acf-json';;
+        return $paths;
+    }
+
+    // (Optional) Hide the ACF admin menu item.
+    add_filter('acf/settings/show_admin', 'my_acf_settings_show_admin');
+    function my_acf_settings_show_admin( $show_admin ) {
+        return false;
+    }
+}
+
+grnd_acf();
