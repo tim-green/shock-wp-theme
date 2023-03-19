f4885fba5d89dd8d905041e6ca1e3b1ef95c807d
Tim <github@timgreen.ws>
2023-03-20 09:49:08 +1100

    removed theme_version

--- inc/enqueues.php
+++ inc/enqueues.php
@@ -22,7 +22,7 @@ if ( ! function_exists( 'grnd_styles_scripts' ) ) {
 		wp_enqueue_script( 'font-awesome', '//kit.fontawesome.com/f8466e44e0.js', false, $theme_version, true );
 
 
-		wp_enqueue_script( 'grnd-scripts', get_template_directory_uri() . '/assets/build/app.min.js', false, $theme_version, true );
+		wp_enqueue_script( 'grnd-scripts', get_template_directory_uri() . '/assets/build/app.min.js');
 
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' ); }
