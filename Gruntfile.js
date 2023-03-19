2676bcc96b08d00075ba2f1da9ba65a257acc703
Tim <github@timgreen.ws>
2023-03-20 09:48:58 +1100

    updated gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,7 +30,9 @@ module.exports = function(grunt) {
       dist: {
         files: {
           'assets/build/app.min.js': [
-            'assets/js/vendor/*.js','assets/js/plugins/*.js','assets/js/**/*.js'
+            'assets/js/vendor/navbar.js',
+            'assets/js/plugins/*.js',
+            'assets/js/**/*.js'
           ]
         },
         options: {
